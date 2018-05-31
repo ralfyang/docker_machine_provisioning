1b7111228db1eb1fe716f1a28ad9d911d16d389d
Ralf Yang <goody80762@gmail.com>
2018-06-01 08:47:49 +0900

    Update mgmt.sh

--- mgmt.sh
+++ mgmt.sh
@@ -21,8 +21,8 @@ Base_cmd="/usr/local/bin/docker-machine"
 
 $Base_cmd 2> /tmp/chk_perm
 	if [[ $(grep Permission /tmp/chk_perm) != "" ]];then
-		eacho " Please run with 'sudo' as below"
-		eacho " 'sudo mgmt.sh'"
+		echo " Please run with 'sudo' as below"
+		echo " 'sudo mgmt.sh'"
 		rm -f /tmp/chk_perm
 		exit 1
 	fi
